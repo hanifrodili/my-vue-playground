dc04e279836a4a71925ea7ad8a4c9e81f8170a9b
Hanif Rodili <hanifrodili@gmail.com>
2021-07-18 22:56:10 +0800

    add upload/download json file

--- src/components/yezzabuilder/builder/MenuBar.vue
+++ src/components/yezzabuilder/builder/MenuBar.vue
@@ -1,17 +1,104 @@
 <template lang="pug">
   v-app-bar(dense dark app style="z-index:150;" :style="$vuetify.breakpoint.width < 960 ? 'margin-top:54px !important' : 'margin-top:64px !important'")
     v-spacer
+    v-tooltip(bottom)
+      template( v-slot:activator="{ on, attrs }" )
+        v-btn(icon @click="downloadJSON" v-bind="attrs" v-on="on")
+            v-icon mdi-download
+      span Download Layout File
+    v-tooltip(bottom)
+      template( v-slot:activator="{ on, attrs }" )
+        v-btn(icon @click="dialog=true" v-bind="attrs" v-on="on")
+            v-icon mdi-upload
+      span Upload Layout File
     router-link(to="/yezzabuilder/preview" target="_blank" )
-      v-btn(icon)
-        v-icon mdi-eye-outline
+      v-tooltip(bottom)
+        template( v-slot:activator="{ on, attrs }" )
+          v-btn(icon  v-bind="attrs" v-on="on")
+            v-icon mdi-eye-outline
+        span Preview Layout
+    v-dialog( v-model="dialog" max-width="500px" )
+      v-card(  )
+        v-card-title
+          h5 Upload Layout File
+        v-card-text()
+          v-container(  )
+            v-row
+              v-col(cols="12")
+                v-file-input(label="Choose Layout File" filled rounded hide-details="auto" accept="application/json" v-model="importedFile" )
+        v-card-actions
+          v-spacer
+          v-btn( text @click="dialog=false") Close
+          v-btn(text @click="readFile") Upload
 </template>
 <script>
 export default {
-  name: 'MenuBar'
+  name: 'MenuBar',
+  data:()=>({
+    dialog: false,
+    importedFile: null
+  }),
+  mounted(){
+  },
+  methods:{
+    downloadJSON(){
+      const data =  window.localStorage.getItem('userComponents')
+      const id =  window.localStorage.getItem('browserID')
+      const blob = new Blob([data], {type: 'application/json'})
+      const e = document.createEvent('MouseEvents'),
+      a = document.createElement('a');
+      a.download = id+"-page-cofig.json";
+      a.href = window.URL.createObjectURL(blob);
+      a.dataset.downloadurl = ['application/json', a.download, a.href].join(':');
+      e.initEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+      a.dispatchEvent(e);
+    },
+    readFile() {
+      let reader = new FileReader();
+      reader.onload = e => {
+        this.$emit('import',e.target.result)
+      };
+      reader.readAsText(this.importedFile);
+      this.importedFile = null
+      this.dialog = false
+    }
+  }
 }
 </script>
 <style scoped>
 a{
   text-decoration: none;
+}
+::v-deep .v-text-field--filled > .v-input__control > .v-input__slot{
+  background: #F7F7F7 !important;
+  border: 1px solid #eaeaea !important;
+  border-radius: 4px;
+  max-height: 48px;
+  min-height: 48px;
+  padding-left: 16px;
+  overflow: hidden;
+}
+::v-deep .v-text-field.v-input--has-state > .v-input__control > .v-input__slot{
+  border: 1px solid red !important;
+}
+::v-deep .v-text-field--filled:not(.v-text-field--single-line) input{
+  margin-top: 14px;
+}
+::v-deep .v-text-field--filled .v-label{
+  top: 14px !important;
+  color: #C1C1C1;
+}
+::v-deep .v-text-field--filled .v-label--active{
+  transform: translateY(-10px) scale(0.75);
+  color: #C1C1C1 ;
+}
+::v-deep .theme--light.v-input input, .theme--light.v-input textarea{
+  color: #2B2525 ;
+  font-weight: 600 !important;
+  font-size: 15px !important;
+  line-height: 24px !important;
+}
+::v-deep .v-input--has-state.error--text{
+  color: red;
 }
 </style>
--- src/views/YezzaBuilder/index.vue
+++ src/views/YezzaBuilder/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .yezza-buider
-    menu-bar()
+    menu-bar(@import="importData")
     div(:style="$vuetify.breakpoint.width < 960 ? 'margin-top:46px' : 'margin-top:48px'")
       template(v-for="(components, index) in userComponents")
         component(:is="components.component" :configs="components.config" 
@@ -25,118 +25,16 @@ export default {
   name: 'YezzaBuilder',
   components: { AddDialog, MenuBar, ...YezzaBlocks },
   data: ()=>({
-    userComponents: [
-      // {
-      //   _uid: "yz-00001",
-      //   component: "Intro",
-      //   config: {
-      //     title: {
-      //       value: 'Tell people about what makes your site special.'
-      //     },
-      //     btnText: {
-      //       value: 'See More'
-      //     },
-      //     btnBgColor: {
-      //       value: '#000000'
-      //     },
-      //     btnTextColor: {
-      //       value: '#FFFFFF'
-      //     },
-      //     btnBorderRadius: {
-      //       value: '4px'
-      //     },
-      //     textColor: {
-      //       value: '#FFFFFF'
-      //     },
-      //     bgColor: {
-      //       value: '#FFFFFF'
-      //     },
-      //     bgImage: {
-      //       value: require('@/components/yezzabuilder/blocks/img/img-a.jpg')
-      //     },
-      //     blockPaddingTop: {
-      //       value: 150
-      //     },
-      //     blockPaddingBottom: {
-      //       value: 150
-      //     },
-      //     bgParallax: {
-      //       value: true
-      //     }
-      //   }
-      // },
-      // {
-      //   _uid: "yz-00002",
-      //   component: "TextBlock",
-      //   config:{
-      //     title: {
-      //       value: 'Your Text Goes Here'
-      //     },
-      //     desc: {
-      //       value: 'Your Long Descriptions Goes Here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-      //     },
-      //     textColor: {
-      //       value: '#000000'
-      //     },
-      //     bgColor: {
-      //       value: '#FFFFFF'
-      //     },
-      //     blockPaddingTop: {
-      //       value: 80
-      //     },
-      //     blockPaddingBottom: {
-      //       value: 80
-      //     }
-      //   }
-      // },
-      // {
-      //   _uid: "yz-00003",
-      //   component: "TextImage",
-      //   config:{
-      //     layout: {
-      //       value: 1
-      //     },
-      //     title: {
-      //       value: 'Your Text Goes Here'
-      //     },
-      //     desc: {
-      //       value: 'Your Long Descriptions Goes Here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-      //     },
-      //     btnText: {
-      //       value: 'See More'
-      //     },
-      //     btnBgColor: {
-      //       value: '#000000FF'
-      //     },
-      //     btnTextColor: {
-      //       value: '#FFFFFFFF'
-      //     },
-      //     btnBorderRadius: {
-      //       value: '4px'
-      //     },
-      //     textColor: {
-      //       value: '#000000FF'
-      //     },
-      //     bgColor: {
-      //       value: '#FFFFFFFF'
-      //     },
-      //     image: {
-      //       value: require('@/components/yezzabuilder/blocks/img/img-b.jpg')
-      //     },
-      //     blockPaddingTop: {
-      //       value: 80
-      //     },
-      //     blockPaddingBottom: {
-      //       value: 80
-      //     }
-      //   }
-      // }
-    ],
+    userComponents: [],
   }),
   mounted(){
     const savedComponents = window.localStorage.getItem('userComponents')
     if (savedComponents !== null && savedComponents !== ''){
       this.userComponents = JSON.parse(savedComponents)
+    }
+    const browserID = window.localStorage.getItem('browserID')
+    if (browserID == null || browserID == ''){
+       window.localStorage.setItem('browserID','user-'+this.randID(10))
     }
   },
   methods: {
@@ -158,7 +56,7 @@ export default {
     },
     addBlock(pos, block){
       let newBlock = ''
-      let newBlockID = this.randID()
+      let newBlockID = this.randID(10)
       const intro = `{ "_uid": "${newBlockID}", "component": "Intro", "config": { "title": { "value": "Tell people about what makes your site special." }, "btnText": { "value": "See More" }, "btnBgColor": { "value": "#000000" }, "btnTextColor": { "value": "#FFFFFF" }, "btnBorderRadius": { "value": "4px" }, "textColor": { "value": "#FFFFFF" }, "bgColor": { "value": "#FFFFFF" }, "bgImage": { "value": "${require("@/components/yezzabuilder/blocks/img/img-a.jpg")}" }, "blockPaddingTop": { "value": 150 }, "blockPaddingBottom": { "value": 150 }, "bgParallax": { "value": true } } }`
       const textBlock = `{ "_uid": "${newBlockID}", "component": "TextBlock", "config": { "title": { "value": "Your Text Goes Here." }, "desc": {"value": "Your Long Descriptions Goes Here. Lorem ipsum dolor sit amet, consectetur adipiscing elit." }, "textColor": { "value": "#000000" }, "bgColor": { "value": "#FFFFFF" }, "blockPaddingTop": { "value": 80 }, "blockPaddingBottom": { "value": 80 } } }`
       const textImage = `{ "_uid": "${newBlockID}", "component": "TextImage", "config":{ "layout": { "value": 1 }, "title": { "value": "Your Text Goes Here" }, "desc": { "value": "Your Long Descriptions Goes Here. Lorem ipsum dolor sit amet, consectetur adipiscing elit." }, "btnText": { "value": "See More" }, "btnBgColor": { "value": "#000000FF" }, "btnTextColor": { "value": "#FFFFFFFF" }, "btnBorderRadius": { "value": "4px" }, "textColor": { "value": "#000000FF" }, "bgColor": { "value": "#FFFFFFFF" }, "image": { "value": "${require('@/components/yezzabuilder/blocks/img/img-b.jpg')}" }, "blockPaddingTop": { "value": 80 }, "blockPaddingBottom": { "value": 80 } } }`
@@ -190,8 +88,8 @@ export default {
       this.userComponents.splice(index+1, 0, element);
       window.localStorage.setItem('userComponents',JSON.stringify(this.userComponents))
     },
-    randID(){
-      var length = 10
+    randID(len){
+      var length = len
       var result           = '';
       var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
       var charactersLength = characters.length;
@@ -200,6 +98,10 @@ export default {
         charactersLength));
       }
       return result;
+    },
+    importData(e){
+      this.userComponents = JSON.parse(e)
+      window.localStorage.setItem('userComponents',JSON.stringify(this.userComponents))
     }
   }
 }
