aff4acdb9ddaefb89f444224e6eba193277e73ac
Mohd Hanif Mohamod Rodili <hanifrodili@gmail.com>
2021-09-13 15:13:56 +0800

    update test display

--- src/components/mycustom/customSchedulePicker.vue
+++ src/components/mycustom/customSchedulePicker.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .my-schedule-picker
     y-schedule-picker.mb-2(v-model="dateTime" :time-interval="timeInterval" :days-count="days" :opening-hour="openingHour" width="100%")
-    p.text-center {{dateTime}}
+    p.text-center {{dateTime ? dateTime: 'Not Selected'}}
 </template>
 <script>
 import ySchedulePicker from '@/components/mycustom/ySchedulePicker.vue'
