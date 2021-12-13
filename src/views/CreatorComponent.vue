e8319537913592aa4ef53c2692f999889fac0516
Mohd Hanif Mohamod Rodili <hanifrodili@gmail.com>
2021-12-13 11:00:55 +0800

    update

--- src/views/CreatorComponent.vue
+++ src/views/CreatorComponent.vue
@@ -1,34 +1,44 @@
 <template lang="pug">
 .creator-component
   //- product-card.mb-2(v-for="(product, index) in products" :key="index" :productData="product" :productIndex="index")
-  product-card-2(:products="products")
+  product-card(:products="products")
 </template>
 
 <script>
-import ProductCard from '@/components/CreatorComponents/ProductCard.vue';
-import ProductCard2 from '@/components/CreatorComponents/ProductCardV2.vue';
+// import ProductCard from '@/components/CreatorComponents/ProductCard.vue';
+import ProductCard from '@/components/CreatorComponents/ProductCardV2.vue';
 
 export default {
   name: 'CreatorComponent',
-  components: { ProductCard, ProductCard2 },
+  components: { ProductCard },
   data: ()=>({
     products:[
       {
+        medium: 'instagram',
         title: 'Instagram Story',
         currency: 'RM',
         rate: 900,
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis scelerisque magna, consectetur elementum nisi. Integer et arcu ante. Aliquam erat volutpat.'
       },
       {
-        title: 'Instagram Feed',
+        medium: 'twitter',
+        title: 'Tweet with image',
         currency: 'RM',
-        rate: 700,
+        rate: 500,
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis scelerisque magna.'
       },
       {
-        title: 'Instagram Video',
+        medium: 'tiktok',
+        title: 'Tiktok video create by me',
         currency: 'RM',
         rate: 1000,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis scelerisque magna, consectetur elementum nisi. Integer et arcu ante. Aliquam erat volutpat. Donec gravida molestie mauris, vitae placerat libero pharetra in. Ut malesuada nibh ante, ac scelerisque lorem elementum in.'
+      },
+      {
+        medium: 'youtube',
+        title: 'Video create by you',
+        currency: 'RM',
+        rate: 500,
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis scelerisque magna, consectetur elementum nisi. Integer et arcu ante. Aliquam erat volutpat. Donec gravida molestie mauris, vitae placerat libero pharetra in. Ut malesuada nibh ante, ac scelerisque lorem elementum in.'
       }
     ]
