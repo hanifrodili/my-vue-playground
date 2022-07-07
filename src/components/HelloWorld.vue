cf1073e45837ecc8ceb827bf1c642609130ae098
Hanif Rodili <hanifrodili@gmail.com>
2022-07-07 12:12:12 +0800

    Update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     h1.text-center.d-flex.flex-column.justify-center(style="font-size:35px; line-height:35px")
       | Welcome to
       br 
-      | My Playground
+      | My Vue Playground
     a(href="https://hanifrodili.com" )
       div.mx-auto.my-5.pa-3(style="background: #2f3765; width: 150px; border-radius: 150px")
         v-img.mx-auto( src="https://ik.imagekit.io/hanifrodili/tr:w-150/the-coder-snapback-03_rX6H5O1xS.png?updatedAt=1629884886670" width="100px" )
